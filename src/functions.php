0d4b8e2457dd072ad31bdfca9302593cde0aa72a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-09-21 12:51:55 +0200

    added ProviderRoute

--- src/functions.php
+++ src/functions.php
@@ -10,6 +10,9 @@ require_once "Routes/States.php";
 require_once "Shipments.php";
 require_once "Routes/Shipments.php";
 
+require_once "ProviderRoute.php";
+require_once "Routes/ProviderRoute.php";
+
 // if (class_exists("Tualo\Office\Dako\Middlewares\Middleware")){ }
 // if (class_exists("Tualo\Office\Dako\Routes\Route")){ }
 
