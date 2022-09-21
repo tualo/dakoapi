0ebbb1acc2068e2a045575ec6bbb39c0589dc0cd
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-09-21 12:42:33 +0200

    format code

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -10,33 +10,28 @@ use Tualo\Office\Dako\DakoSend;
 class Route implements IRoute{
     public static function register(){
 
-       BasicRoute::add('/dako/products',function($matches){
-        App::contenttype('application/json');
-        try{
-            DakoSend::init();
-            App::result('products',DakoSend::products());
-            App::result('success',true);
-        }catch(\Exception $e){
-            App::result('msg',$e->getMessage());
-            App::result('success',true);
-        }
-    },array('get','post'),true);
+        BasicRoute::add('/dako/products',function($matches){
+            App::contenttype('application/json');
+            try{
+                DakoSend::init();
+                App::result('products',DakoSend::products());
+                App::result('success',true);
+            }catch(\Exception $e){
+                App::result('msg',$e->getMessage());
+                App::result('success',true);
+            }
+        },array('get','post'),true);
 
-
-
-       BasicRoute::add('/dako/services',function($matches){
-        App::contenttype('application/json');
-        try{
-            DakoSend::init();
-            App::result('services',DakoSend::services());
-            App::result('success',true);
-        }catch(\Exception $e){
-            App::result('msg',$e->getMessage());
-            App::result('success',true);
-        }
-    },array('get','post'),true);
-
-
-
+        BasicRoute::add('/dako/services',function($matches){
+            App::contenttype('application/json');
+            try{
+                DakoSend::init();
+                App::result('services',DakoSend::services());
+                App::result('success',true);
+            }catch(\Exception $e){
+                App::result('msg',$e->getMessage());
+                App::result('success',true);
+            }
+        },array('get','post'),true);
     }
 }
