cde74d9c6a5def8b4ce494d13442cb283ac66801
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-09-21 08:47:36 +0200

    fix namings

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once "RequestHelper.php";
+require_once "DakoSend.php";
+require_once "Routes\Route.php";
+
 // if (class_exists("Tualo\Office\Dako\Middlewares\Middleware")){ }
-if (class_exists("Tualo\Office\Dako\Routes\Route")){ }
+// if (class_exists("Tualo\Office\Dako\Routes\Route")){ }
 
