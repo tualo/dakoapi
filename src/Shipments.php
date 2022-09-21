ac295d97be2b2373056051e08eb3baf2055e933b
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-09-21 11:18:14 +0200

    fix page size

--- src/Shipments.php
+++ src/Shipments.php
@@ -11,7 +11,7 @@ use Tualo\Office\Basic\IRoute;
 class Shipments extends DakoSend {
     public static  $shipmentsURL='/index.php/api/tracking/shipments/X-API-KEY/{api_key}/api_secret/{api_secret}'; // url aus doku
     public static function shipments(
-        $page='0'
+        $page='1'
     ){
 
         $result = RequestHelper::query(self::prepareURL( self::$shipmentsURL).'?page='.$page);
