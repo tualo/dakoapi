b296f39f9f03358beeb6e3bc096028fb39c5954d
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-09-21 12:04:34 +0200

    New Class shipmentInfo

---
+++ src/Routes/ShipmentInfo.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tualo\Office\Dako\Routes;
+
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\IRoute;
+use Tualo\Office\Dako\ShipmentInfo as DShipmentInfo; 
+
+
+class ShipmentInfo implements IRoute{
+    public static function register(){
+
+       BasicRoute::add('/dako/shipmentinfo',function($matches){
+        DShipmentInfo::init();
+        App::contenttype('application/json');
+        App::result('shipmentInfo',DShipmentInfo::shipmentInfo());
+       });
+
+    }
+}
+
